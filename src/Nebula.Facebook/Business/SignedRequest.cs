0a2e9e698ca810202e2d75516c93c6fa6d548c3f
Joshua Williams <jjoshau@gmail.com>
2013-09-25 16:57:09 -0400

    removed helper data

--- src/Nebula.Facebook/Business/SignedRequest.cs
+++ src/Nebula.Facebook/Business/SignedRequest.cs
@@ -6,26 +6,6 @@ using System.Text;
 namespace Nebula.Facebook.Business {
     public class SignedRequest {
 
-        //{
-        //   "algorithm": "HMAC-SHA256",
-        //   "expires": 1380128400,
-        //   "issued_at": 1380123830,
-        //   "oauth_token": "CAAGTY6ddm3ABAKL8N7m2ZAdMhIpv2twUmLCgALNMzKFZCy4f2lPXAZAAhkZBbMtWFJqIq3S6hYmo0Cwmwv7GlsQotx32pe2CBpyxIBIJQeCObZC2SRFpQG4uqoVBvwFBFzDZA847OY0OpujnWubV0rz1M0PTamoqO2XSkEOTpCwd0M52qBAhM59iIOYjUQUB2TUMioVbypiAZDZD",
-        //   "page": {
-        //      "id": "111014669977",
-        //      "liked": false,
-        //      "admin": true
-        //   },
-        //   "user": {
-        //      "country": "us",
-        //      "locale": "en_US",
-        //      "age": {
-        //         "min": 21
-        //      }
-        //   },
-        //   "user_id": "100001486925330"
-        //}
-
         public SignedRequestUser user { get; set; }
         public string algorithm { get; set; }
         public double issued_at { get; set; }
