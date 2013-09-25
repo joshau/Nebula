ef20c27690f79128ca60a14bfff63761515a732e
Joshua Williams <jjoshau@gmail.com>
2013-09-25 17:12:48 -0400

    added missing class

---
+++ src/Nebula.Facebook/Business/SignedRequest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Nebula.Facebook.Business {
+    public class SignedRequest {
+
+        public SignedRequestUser user { get; set; }
+        public string algorithm { get; set; }
+        public double issued_at { get; set; }
+        public long user_id { get; set; }
+        public string oauth_token { get; set; }
+        public double expires { get; set; }
+        public string app_data { get; set; }
+        public SignedRequestPage page { get; set; }        
+
+        public static SignedRequest Create(string signedRequest, string facebookAppSecretKey) {
+
+            if (signedRequest == null) return null;
+
+            string[] signedRequestSplit = signedRequest.Split('.');
+            string encodedSignature = signedRequestSplit[0];
+            string payload = signedRequestSplit[1];
+            string signature = Utility.Base64UrlDecode(encodedSignature);
+            string data = Utility.Base64UrlDecode(payload);
+            SignedRequest signReq = Utility.DeserializeSignedRequest(data);
+
+            if(signReq.algorithm.ToUpper() != "HMAC-SHA256") {
+                throw new Exceptions.InvalidAlgorithmException(String.Format("Unknown algorithm ({0}). Expected {1}.", signReq.algorithm.ToUpper(), "HMAC-SHA256"));
+            }
+            if(signature != Utility.Base64UrlDecode(Utility.GeneratorSignatureHash(payload, facebookAppSecretKey))) {
+                throw new Exceptions.InvalidSignedRequestException("Bad JSON signature!");
+            }
+
+            return signReq;
+        }
+    }
+}
