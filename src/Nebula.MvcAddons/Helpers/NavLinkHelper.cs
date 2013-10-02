5cd6969d6a2b5d272307e4b9a3b9dd418aa6c6ce
Joshua Williams <jjoshau@gmail.com>
2013-10-02 09:49:06 -0400

    added nav list item

--- src/Nebula.MvcAddons/Helpers/NavLinkHelper.cs
+++ src/Nebula.MvcAddons/Helpers/NavLinkHelper.cs
@@ -16,5 +16,19 @@ namespace Nebula.MvcAddons.Helpers {
                 url.Action(action, controller),
                 text));
         }
+
+        public static IHtmlString NavLinkListItem(this HtmlHelper helper, string action, string controller, string text, bool matchControllerOnly = false) {
+
+            string currentArea = helper.ViewContext.RouteData.DataTokens["area"] == null ? "" : helper.ViewContext.RouteData.DataTokens["area"].ToString();
+            string currentAction = HttpContext.Current.Request.RequestContext.RouteData.Values["action"].ToString();
+            string currentController = HttpContext.Current.Request.RequestContext.RouteData.Values["controller"].ToString();
+
+            UrlHelper url = new UrlHelper(HttpContext.Current.Request.RequestContext);
+
+            return MvcHtmlString.Create(string.Format("<li class=\"{0}\"><a href=\"{1}\">{2}</a></li>",
+                (matchControllerOnly || action.ToLower() == currentAction.ToLower()) && controller.ToLower() == currentController.ToLower() ? "active" : "",
+                url.Action(action, controller),
+                text));
+        }   
     }
 }
